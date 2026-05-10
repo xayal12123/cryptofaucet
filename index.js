9d8795cf33949728b0c2b502b1d3e0dbde0710e6
xayal12123 <xayalamanov121@gmail.com>
2026-05-10 21:00:02 +0400

    index.js
    Changed import statement to use 'uygulama.js' instead of 'app.js'.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app.js';
+import App from './uygulama.js'; // Adın düzgün olduğundan əmin oluruq
 
 ReactDOM.render(<App />, document.getElementById('app'));
